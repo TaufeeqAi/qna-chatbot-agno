f9c81d9101864468edc4696e058be1e8fa992469
Taufeeque Ahmad <63660975+TaufeeqAi@users.noreply.github.com>
2025-05-17 11:36:47 +0530

    projetct setup complete

---
+++ backend/app/core/config.py
@@ -0,0 +1,11 @@
+from pydantic import BaseSettings
+
+class Settings(BaseSettings):
+    DATABASE_URL: str
+    SECRET_KEY: str
+    ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
+    VECTOR_DB_URL: str
+
+    class Config:
+        env_file = ".env"
